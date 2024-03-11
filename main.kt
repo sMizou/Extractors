a00a5094364617f4794ba50cab20fef3e0572b81
sMizou <158730900+sMizou@users.noreply.github.com>
2024-03-11 21:45:12 +0100

    Update main.kt
    we need a Triple to return the final result

--- main.kt
+++ main.kt
@@ -1,1 +1,11 @@
 
+// a data class to return $originalUrl, $refererUrl, $downloadUrl
+data class Triple<out A, out B, out C>(
+    val originalUrl: A,
+    val refererUrl: B,
+    val downloadUrl: C
+) : Serializable {
+
+    override fun toString(): String = "($originalUrl, $refererUrl, $downloadUrl)"
+
+}
